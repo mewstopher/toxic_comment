7fc5a74ad1c2f06d218268930d5277f53c3c4bd4
Mu <mu@email.com>
2019-07-16 17:53:19 -0400

    add option to load model

--- code/run_mod.py
+++ code/run_mod.py
@@ -24,17 +24,25 @@ X_train, X_test, y_train, y_test = train_test_split(X_padded, Y)
 
 # for running as script
 if __name__ == "__main__":
-    train_obs = input("number of observations to use for testing(type 'a' for all): ")
-    if train_obs == 'a':
-        train_obs = X_train.shape[0]
+    load_or_save = input("press l to load model, press t to train from scratch: ")
+    if load_or_save == 't':
+        train_obs = input("number of observations to use for testing(type 'a' for all): ")
+        if train_obs == 'a':
+            train_obs = X_train.shape[0]
+        else:
+            train_obs = int(train_obs)
+        model = trial_lstm((200,), word_to_vec_map, word_to_index, trainable=True)
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        model.fit(X_train[0:train_obs], y_train[0:train_obs], epochs=1, batch_size=32, shuffle=True)
     else:
-        train_obs = int(train_obs)
-    model = trial_lstm((200,), word_to_vec_map, word_to_index, trainable=True)
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    model.fit(X_train[0:train_obs], y_train[0:train_obs], epochs=1, batch_size=32, shuffle=True)
+        model_to_load = input("type in model name to load: ")
+        model = load_model("../output/" + model_to_load)
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']) 
+        model.fit(X_train[0:train_obs], y_train[0:train_obs], epochs=1, batch_size=32, shuffle=True)
     do_save = input("save model? press y to save: ")
     if do_save == 'y':
-        model.save("../output/newmodel")
+        model_name = input("type in name of new model to save: ")
+        model.save("../output/" + model_name)
     do_test = input("test model? press y to test: ")
     if do_test == 'y':
         get_test_accuracy(X_test, y_test, model=model)

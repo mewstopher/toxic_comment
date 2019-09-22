0c88dbaf6f5edda666f866fb47ed214305e2c636
Mu <mu@email.com>
2019-09-22 16:45:46 -0400

    remove old files, add pipfile and update imports

--- code/imports.py
+++ code/imports.py
@@ -1,14 +1,12 @@
 import pandas as pd
 import numpy as np
 from string import punctuation
-from keras.models import Model
-from keras.layers import Dense, Input, Dropout, LSTM, Activation
-from keras.layers.embeddings import Embedding
-from keras.preprocessing import sequence
-from keras.initializers import glorot_uniform
 from sklearn.model_selection import train_test_split
-from keras.preprocessing.text import text_to_word_sequence
-from keras.preprocessing.sequence import pad_sequences
-from keras.layers import Bidirectional, GlobalMaxPool1D
-from keras.models import load_model
+import torch
+from torch.utils import data
+from torch.nn import functional as F
+from torch import autograd
+import sys
+sys.append("../../")
+import matplotlib as plt
 import os
